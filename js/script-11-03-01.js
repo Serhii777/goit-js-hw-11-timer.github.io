567cea19fb01f4e7366f9989b45d98be885ad14b
Serhii777 <muzykasv72@gmail.com>
2020-07-09 18:35:59 +0300

    Added task-03

--- js/script-11-03-01.js
+++ js/script-11-03-01.js
@@ -1,29 +1,40 @@
-new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
-  });
+let end_date = {
+  full_year: "2020", // Год
+  month: "07", // Номер месяца
+  day: "10", // День
+  hours: "18", // Час
+  minutes: "30", // Минуты
+  seconds: "10", // Секунды
+};
 
-  /*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const refs = {
+  timerRef: document.querySelector("#timer-1"),
+  daysRef: document.querySelector('span[data-value="days"]'),
+  hoursRef: document.querySelector('span[data-value="hours"]'),
+  minuteslRef: document.querySelector('span[data-value="mins"]'),
+  secondsRef: document.querySelector('span[data-value="secs"]'),
+};
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const timer = setInterval(function () {
+  let currentTime = new Date().getTime();
+  const endDate = new Date(
+    `${end_date.full_year}-${end_date.month}-${end_date.day}T${end_date.hours}:${end_date.minutes}:${end_date.seconds}`
+  ).getTime();
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  let time = endDate - currentTime;
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+  if (time >= 0) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    refs.daysRef.textContent = days;
+    refs.hoursRef.textContent = ("0" + hours).slice(-2);
+    refs.minuteslRef.textContent = ("0" + mins).slice(-2);
+    refs.secondsRef.textContent = ("0" + secs).slice(-2);
+  } else {
+    refs.timerRef.classList.add("js-timer");
+    refs.timerRef.textContent = "Обратный отсчет окончен!";
+  }
+}, 1000);
